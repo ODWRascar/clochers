e34205c0c8c65e676bec5fb8e36a0512153bfdf0
ODWRascar <tonymathurel@gmail.com>
2025-08-17 00:16:57 +0200

    Update kv.ts

--- clochers-du-frontonnais-prototype/lib/kv.ts
+++ clochers-du-frontonnais-prototype/lib/kv.ts
@@ -1,12 +1,3 @@
-import { createClient } from 'redis';
-
-const redis = createClient({
-  url: process.env.KV_REST_API_URL, // URL fournie par Vercel KV
-  password: process.env.KV_REST_API_TOKEN // Token fourni par Vercel KV
-});
-
-redis.on('error', (err) => console.error('Redis Client Error', err));
-
-await redis.connect();
-
-export default redis;
+// lib/kv.ts
+import { kv } from '@vercel/kv';
+export default kv;
