62beefe432f6df3c2db94b5ad8aeabef72b42ebd
ODWRascar <tonymathurel@gmail.com>
2025-08-17 00:22:06 +0200

    Update route.ts

--- clochers-du-frontonnais-prototype/app/api/auth/me/route.ts
+++ clochers-du-frontonnais-prototype/app/api/auth/me/route.ts
@@ -1,4 +1,6 @@
 // app/api/auth/me/route.ts
+export const runtime = 'nodejs';
+
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { verifySession } from "../../../../lib/auth";
